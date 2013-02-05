fb5409244ae7b5621e2fdfeb25e897a84a6436db
Szabolcs Horvát <szhorvat@gmail.com>
2013-02-04 21:04:02 -0500

    Minor installer cleanup

--- SEUploaderInstaller.m
+++ SEUploaderInstaller.m
@@ -2,17 +2,16 @@
 
 If[$VersionNumber < 8, 
    Print["SE Uploader requires Mathematica 8 or higher"],
-(* get the Installer:*)
 
-Import["http://packageinstaller.googlecode.com/hg/PackageInstaller/PackageInstaller.m"]; 
+(* get the Installer:*)
+   Import["http://packageinstaller.googlecode.com/hg/PackageInstaller/PackageInstaller.m"]; 
 
 (* this is the location of the uploader as advertised here:
-http://meta.mathematica.stackexchange.com/questions/5/can-i-easily-post-images-to-this-site-directly-from-mathematica-yes*)
+   http://meta.mathematica.stackexchange.com/a/32 *)
 
 (* this function will copy the palette locally  to 
    FileNameJoin[{$UserBaseDirectory, "SystemFiles", "FrontEnd", "Palettes", "SE Uploader.nb"}] 
-and open  it 
-*)
+   and open it  *)
 
-PackageInstaller`InstallPalette["https://raw.github.com/szhorvat/SEUploader/master/SE%20Uploader.nb"];
+    PackageInstaller`InstallPalette["https://raw.github.com/szhorvat/SEUploader/master/SE%20Uploader.nb"];
 ]
